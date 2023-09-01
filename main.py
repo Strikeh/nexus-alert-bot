8a8134db0397f1267fcfc9fad7729990e0024f9b
Christophe <christophe.verheyen@trivec.be>
2023-09-01 10:15:51 +0200

    Use parsemode instead of string

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import config
 import logging
 import threading
 from pyrogram import Client, filters
+from telegram import ParseMode
 
 from utils import calc_lost_blocks, get_block, get_latest_block, process_block
 dotenv.load_dotenv()
@@ -92,11 +93,12 @@ def whale_notifier(main_context):
         messages, errors = process_block(block)
         for msg in messages:
             if config.DEBUG_MODE:
-                bot.send_message(config.DEVELOPER_CHAT_ID, msg)
+                bot.send_message(config.DEVELOPER_CHAT_ID,
+                                 msg, parse_mode=ParseMode.HTML)
             else:
                 bot.send_message(config.ALERT_CHANNEL_ID, msg,
                                  reply_to_message_id=config.MESSAGE_THREAD_ID,
-                                 parse_mode='HTML')
+                                 parse_mode=ParseMode.HTML)
         for err in errors:
             send_notifications(err)
 
